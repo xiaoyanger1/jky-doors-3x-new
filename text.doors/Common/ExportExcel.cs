5ceb9429a64d61404b1aa9368e7c19502bb7b32d
张扬 <zhangyang@53zaixian.com>
2020-12-18 10:17:52 +0800

    1
    1

--- text.doors/Common/ExportExcel.cs
+++ text.doors/Common/ExportExcel.cs
@@ -39,63 +39,61 @@ namespace text.doors.Common
 
             #region   --性能检测报告--
             ws = (NPOI.HSSF.UserModel.HSSFSheet)hssfworkbook.GetSheet("性能检测报告");
+            ws.GetRow(1).GetCell(2).SetCellValue(dt_Settings.Rows[0]["weituobianhao"].ToString());
+            ws.GetRow(1).GetCell(5).SetCellValue(dt_Settings.Rows[0]["dt_Code"].ToString());
+            ws.GetRow(2).GetCell(3).SetCellValue(dt_Settings.Rows[0]["weituodanwei"].ToString());
+            ws.GetRow(3).GetCell(3).SetCellValue(dt_Settings.Rows[0]["dizhi"].ToString());
+            ws.GetRow(3).GetCell(8).SetCellValue(dt_Settings.Rows[0]["dianhua"].ToString());
 
-            ws.GetRow(2).GetCell(3).SetCellValue(dt_Settings.Rows[0]["dt_Code"].ToString());
-            ws.GetRow(3).GetCell(3).SetCellValue(dt_Settings.Rows[0]["weituobianhao"].ToString());
-            ws.GetRow(4).GetCell(3).SetCellValue(dt_Settings.Rows[0]["weituodanwei"].ToString());
-            ws.GetRow(5).GetCell(3).SetCellValue(dt_Settings.Rows[0]["dizhi"].ToString());
-            ws.GetRow(5).GetCell(8).SetCellValue(dt_Settings.Rows[0]["dianhua"].ToString());
-            ws.GetRow(6).GetCell(3).SetCellValue(dt_Settings.Rows[0]["chouyangriqi"].ToString());
-            ws.GetRow(7).GetCell(3).SetCellValue(dt_Settings.Rows[0]["chouyangdidian"].ToString());
-            ws.GetRow(8).GetCell(3).SetCellValue(dt_Settings.Rows[0]["gongchengmingcheng"].ToString());
-            ws.GetRow(9).GetCell(3).SetCellValue(dt_Settings.Rows[0]["shengchandanwei"].ToString());
+            ws.GetRow(4).GetCell(3).SetCellValue(dt_Settings.Rows[0]["chouyangriqi"].ToString());
+            ws.GetRow(5).GetCell(3).SetCellValue(dt_Settings.Rows[0]["chouyangdidian"].ToString());
+            ws.GetRow(6).GetCell(3).SetCellValue(dt_Settings.Rows[0]["gongchengmingcheng"].ToString());
+            ws.GetRow(7).GetCell(3).SetCellValue(dt_Settings.Rows[0]["shengchandanwei"].ToString());
             //样品
-            ws.GetRow(10).GetCell(3).SetCellValue(dt_Settings.Rows[0]["yangpinmingcheng"].ToString());
-            ws.GetRow(10).GetCell(8).SetCellValue(dt_Settings.Rows[0]["yangpinzhuangtai"].ToString());
+            ws.GetRow(8).GetCell(3).SetCellValue(dt_Settings.Rows[0]["yangpinmingcheng"].ToString());
+            ws.GetRow(8).GetCell(8).SetCellValue(dt_Settings.Rows[0]["yangpinzhuangtai"].ToString());
             //ws.GetRow(11).GetCell(3).SetCellValue(dt_Settings.Rows[0]["shengchandanwei"].ToString());
-            ws.GetRow(11).GetCell(8).SetCellValue(dt_Settings.Rows[0]["guigexinghao"].ToString());
+            ws.GetRow(9).GetCell(8).SetCellValue(dt_Settings.Rows[0]["guigexinghao"].ToString());
             //检测
-            ws.GetRow(12).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jiancexiangmu"].ToString());
-            ws.GetRow(12).GetCell(8).SetCellValue(dt_Settings.Rows[0]["jianceshuliang"].ToString());
-            ws.GetRow(13).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jiancedidian"].ToString());
-            ws.GetRow(13).GetCell(8).SetCellValue(dt_Settings.Rows[0]["jianceriqi"].ToString());
-            ws.GetRow(14).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jianceyiju"].ToString());
-            ws.GetRow(15).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jianceshebei"].ToString());
+            ws.GetRow(10).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jiancexiangmu"].ToString());
+            ws.GetRow(10).GetCell(8).SetCellValue(dt_Settings.Rows[0]["jianceshuliang"].ToString());
+            ws.GetRow(11).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jiancedidian"].ToString());
+            ws.GetRow(11).GetCell(8).SetCellValue(dt_Settings.Rows[0]["jianceriqi"].ToString());
+            ws.GetRow(12).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jianceyiju"].ToString());
+            ws.GetRow(13).GetCell(3).SetCellValue(dt_Settings.Rows[0]["jianceshebei"].ToString());
             //结论
+            ws.GetRow(16).GetCell(8).SetCellValue(0);
+            ws.GetRow(17).GetCell(8).SetCellValue(0);
             ws.GetRow(18).GetCell(8).SetCellValue(0);
-            ws.GetRow(19).GetCell(8).SetCellValue(0);
+            ws.GetRow(19).GetCell(4).SetCellValue("(采用稳定加压方法检测)");
             ws.GetRow(20).GetCell(8).SetCellValue(0);
-            ws.GetRow(21).GetCell(4).SetCellValue("(采用稳定加压方法检测)");
-            ws.GetRow(22).GetCell(8).SetCellValue(0);
             #endregion
 
             #region 质量检测报告
             ws = (NPOI.HSSF.UserModel.HSSFSheet)hssfworkbook.GetSheet("质量检测报告");
-            ws.GetRow(2).GetCell(2).SetCellValue(dt_Settings.Rows[0]["dt_Code"].ToString());
-            ws.GetRow(2).GetCell(9).SetCellValue(dt_Settings.Rows[0]["weituobianhao"].ToString());
+            ws.GetRow(2).GetCell(2).SetCellValue(dt_Settings.Rows[0]["weituobianhao"].ToString());
+            ws.GetRow(2).GetCell(6).SetCellValue(dt_Settings.Rows[0]["dt_Code"].ToString());
             ws.GetRow(3).GetCell(2).SetCellValue(dt_Settings.Rows[0]["kaiqifengchang"].ToString());
             ws.GetRow(3).GetCell(9).SetCellValue(dt_Settings.Rows[0]["shijianmianji"].ToString());
             ws.GetRow(4).GetCell(2).SetCellValue(dt_Settings.Rows[0]["mianbanpinzhong"].ToString());
-            ws.GetRow(4).GetCell(9).SetCellValue(dt_Settings.Rows[0]["kaiqifangshi"].ToString());
             ws.GetRow(5).GetCell(2).SetCellValue(dt_Settings.Rows[0]["mianbanxiangqian"].ToString());
             ws.GetRow(5).GetCell(9).SetCellValue(dt_Settings.Rows[0]["kuangshanmifeng"].ToString());
             ws.GetRow(6).GetCell(2).SetCellValue(dt_Settings.Rows[0]["dangqianwendu"].ToString());
             ws.GetRow(6).GetCell(9).SetCellValue(dt_Settings.Rows[0]["daqiyali"].ToString());
+
+
+            ws.GetRow(7).GetCell(3).SetCellValue(""); //宽
+            ws.GetRow(7).GetCell(7).SetCellValue("");//长
+            ws.GetRow(7).GetCell(11).SetCellValue("");// 厚
+
+            //工程设计值
+
+            ws.GetRow(7).GetCell(3).SetCellValue("");
+            ws.GetRow(7).GetCell(7).SetCellValue("");
+            ws.GetRow(7).GetCell(11).SetCellValue("");
+
             #endregion
 
-            #region 质量检测报告
-            ws = (NPOI.HSSF.UserModel.HSSFSheet)hssfworkbook.GetSheet("质量检测报告");
-            ws.GetRow(2).GetCell(2).SetCellValue(dt_Settings.Rows[0]["dt_Code"].ToString());
-            ws.GetRow(2).GetCell(9).SetCellValue(dt_Settings.Rows[0]["weituobianhao"].ToString());
-            ws.GetRow(3).GetCell(2).SetCellValue(dt_Settings.Rows[0]["kaiqifengchang"].ToString());
-            ws.GetRow(3).GetCell(9).SetCellValue(dt_Settings.Rows[0]["shijianmianji"].ToString());
-            ws.GetRow(4).GetCell(2).SetCellValue(dt_Settings.Rows[0]["mianbanpinzhong"].ToString());
-            ws.GetRow(4).GetCell(9).SetCellValue(dt_Settings.Rows[0]["kaiqifangshi"].ToString());
-            ws.GetRow(5).GetCell(2).SetCellValue(dt_Settings.Rows[0]["mianbanxiangqian"].ToString());
-            ws.GetRow(5).GetCell(9).SetCellValue(dt_Settings.Rows[0]["kuangshanmifeng"].ToString());
-            ws.GetRow(6).GetCell(2).SetCellValue(dt_Settings.Rows[0]["dangqianwendu"].ToString());
-            ws.GetRow(6).GetCell(9).SetCellValue(dt_Settings.Rows[0]["daqiyali"].ToString());
-            #endregion
 
             ws.ForceFormulaRecalculation = true;
 
